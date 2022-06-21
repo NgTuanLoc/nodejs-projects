053048ae245568a298b007c00bb5382466225c94
NgTuanLoc <18521011@gm.uit.edu.vn>
2022-06-21 15:11:51 +0700

    task manager project

---
+++ 01-task-manager/app.js
@@ -0,0 +1,34 @@
+import express from "express";
+import { connectDB } from "./db/connect.js";
+import dotenv from "dotenv";
+
+import tasks from "./routers/tasks.js";
+import notFound from "./middlewares/not-found.js";
+import { errorHandlerMiddleware } from "./middlewares/error-handler.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(express.static("./public"));
+app.use(express.json());
+
+// Routes
+app.use("/api/v1/tasks", tasks);
+app.use(notFound);
+app.use(errorHandlerMiddleware);
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(PORT, () => {
+      console.log(`Server is listening on port ${PORT}`.server);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
