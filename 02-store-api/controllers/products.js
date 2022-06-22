8d56908505eede9bd53986a569c9ebe9014423da
NgTuanLoc <18521011@gm.uit.edu.vn>
2022-06-22 15:07:33 +0700

    project 2 completed add numeric filters, field selection

--- 02-store-api/controllers/products.js
+++ 02-store-api/controllers/products.js
@@ -1,12 +1,13 @@
 import Product from "../models/product.js";
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({}).sort("name");
+  const products = await Product.find({}).select("name price");
   res.status(200).json({ products, nbHits: products.length });
 };
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort } = req.query;
+  const { featured, company, name, sort, fields, numericFilters } = req.query;
   const queryObject = {};
+
   if (featured) {
     queryObject.featured = featured === "true" ? true : false;
   }
@@ -16,17 +17,51 @@ const getAllProducts = async (req, res) => {
   if (name) {
     queryObject.name = { $regex: name, $options: "i" };
   }
-
-  let results = Product.find(queryObject);
-  if (sort) {
-    const sortList = sort.split(",").join("");
-    results = results.sort(sortList);
-  } else {
-    results = results.sort("createdAt");
+  if (numericFilters) {
+    const operatorMap = {
+      ">": "$gt",
+      ">=": "$gte",
+      "=": "$eq",
+      "<": "$lt",
+      "<=": "$lte",
+    };
+    const regEx = /\b(<|>|>=|=|<|<=)\b/g;
+    let filters = numericFilters.replace(
+      regEx,
+      (match) => `-${operatorMap[match]}-`
+    );
+    const options = ["price", "rating"];
+    filters = filters.split(",").forEach((item) => {
+      const [field, operator, value] = item.split("-");
+      if (options.includes(field)) {
+        queryObject[field] = { [operator]: Number(value) };
+      }
+    });
   }
-  const products = await results;
 
-  res.status(200).json({ products, nHits: products.length });
+  let result = Product.find(queryObject);
+  // sort
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createdAt");
+  }
+
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ");
+    result = result.select(fieldsList);
+  }
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+  // 23
+  // 4 7 7 7 2
+
+  const products = await result;
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 export { getAllProductsStatic, getAllProducts };
