028a4ad424a34451e2a35f0eddb9226ce28f72a3
NgTuanLoc <18521011@gm.uit.edu.vn>
2022-06-24 20:04:07 +0700

    add security 3rd party middleware

--- 04-jobs-api/app.js
+++ 04-jobs-api/app.js
@@ -3,6 +3,12 @@ import express from 'express';
 import 'express-async-errors';
 import colors from 'colors';
 
+// extra security packages
+import helmet from 'helmet';
+import cors from 'cors';
+import xss from 'xss-clean';
+import rateLimiter from 'express-rate-limit';
+
 // error handler
 import { notFoundMiddleware } from './middleware/not-found.js';
 import { errorHandlerMiddleware } from './middleware/error-handler.js';
@@ -20,6 +26,15 @@ const app = express();
 
 app.use(express.json());
 // extra packages
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.use(
+	rateLimiter({
+		windowMs: 15 * 60 * 1000, // 15 minutes
+		max: 100 // limit each IP to 100 requests per windowMs
+	})
+);
 
 // routes
 app.get('/', (req, res) => {
