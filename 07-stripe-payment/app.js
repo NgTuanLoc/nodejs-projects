d8527bdf0cb1944c457d9002da268f34df5248be
NgTuanLoc <18521011@gm.uit.edu.vn>
2022-06-29 09:54:11 +0700

    project 7 stripe payment completed

---
+++ 07-stripe-payment/app.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import 'express-async-errors';
+
+// error handler
+import { notFound as notFoundMiddleware } from './middleware/not-found.js';
+import { errorHandlerMiddleware } from './middleware/error-handler.js';
+
+dotenv.config();
+
+const app = express();
+
+// controller
+
+app.use(express.json());
+app.use(express.static('./public'));
+
+// stripe
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+	try {
+		app.listen(port, () =>
+			console.log(`Server is listening on port ${port}...`)
+		);
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+start();
---
+++ 07-stripe-payment/errors/index.js
@@ -0,0 +1,6 @@
+import { BadRequestError } from './bad-request.js';
+import { CustomAPIError } from './custom-api.js';
+import { UnauthenticatedError } from './unauthenticated.js';
+import { NotFoundError } from './not-found.js';
+
+export { CustomAPIError, UnauthenticatedError, NotFoundError, BadRequestError };
